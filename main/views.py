3838ac35867cf66f6e78d4d3536f41d59286dc55
StevesInTheNet <stevesinthenet@gmail.com>
2024-12-14 13:04:14 +0300

    Backup

--- main/views.py
+++ main/views.py
@@ -1,4 +1,24 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from io import StringIO
+import sys
 
 def main(request):
-    return render(request, 'main/main.html')
+    if request.method == 'GET':
+        return render(request, 'main/main.html')
+    elif request.method == 'POST':
+        #lang = request.POST.get('lang')
+        code = request.POST.get('code')
+        
+        old_stdout = sys.stdout # Redirecting stdout to save result of exec() into a variable
+        redirected_output = sys.stdout = StringIO()
+
+        try:
+            exec(code)
+        except Exception as e:
+            result = str(e)
+        else:
+            sys.stdout = old_stdout
+            result = redirected_output.getvalue()
+
+        return JsonResponse({'result':result})
--- templates/main/main.js
+++ templates/main/main.js
@@ -4,3 +4,26 @@ editor.session.setMode("ace/mode/python");
 editor.setValue("def foo():\n    print('hello world')\n\nfoo()");
 document.getElementById('editor').style.fontSize='16px';
 document.getElementById('editor').style.marginTop='60px';
+
+
+function run_success(response) {
+    console.log(response);
+}
+
+function run(e) {
+    e.preventDefault();
+    let data = {};
+    data.lang = 'python';
+    data.code = editor.getValue();
+    data.csrfmiddlewaretoken = document.getElementById("csrf").value;
+    $.ajax({
+        type: 'POST',
+        url: '',
+        data: data,
+        success: run_success,
+        error: function(error) {
+            console.log(data);
+            console.log('Error:', error);
+        }
+    });
+}
