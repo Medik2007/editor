e383dda37f62cf638709edd1cec9db5be1184ea3
StevesInTheNet <stevesinthenet@gmail.com>
2024-12-14 14:05:38 +0300

    Backup

--- templates/main/main.js
+++ templates/main/main.js
@@ -1,7 +1,7 @@
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/tomorrow_night");
 editor.session.setMode("ace/mode/python");
-editor.setValue("def foo():\n    print('hello world')\n\nfoo()");
+editor.setValue("print('Hello World')");
 document.getElementById('editor').style.fontSize='16px';
 document.getElementById('editor').style.marginTop='60px';
 
