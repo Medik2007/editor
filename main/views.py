97e0841e6850931597beb3393b65bd26a7ccaf0f
StevesInTheNet <stevesinthenet@gmail.com>
2024-12-14 22:13:10 +0300

    Backup

--- main/views.py
+++ main/views.py
@@ -8,47 +8,37 @@ import tempfile
 def main(request):
     if request.method == 'GET':
         return render(request, 'main/main.html')
+
     elif request.method == 'POST':
         lang = request.POST.get('lang')
         code = request.POST.get('code')
 
         with tempfile.TemporaryDirectory() as temp_dir:
-            file = os.path.join(temp_dir, f'temp_code.{lang}')
+            file = os.path.join(temp_dir, f'temp.{lang}')
 
             with open(file, 'w') as f:
                 f.write(code)
 
-            if lang == 'cpp' or lang == 'c':
-                executable = os.path.join(temp_dir, 'temp_code')
-                if lang == 'cpp':
-                    command = ['g++', file, '-o', executable]
-                else:
-                    command = ['gcc', file, '-o', executable]
+            if lang == 'cpp':
+                executable = os.path.join(temp_dir, 'temp')
+                command = ['g++', file, '-o', executable]
                 compilation = subprocess.run(command, capture_output=True, text=True)
 
                 if compilation.returncode != 0:
-                    return JsonResponse({'error':'compilation', 'result':compilation.stderr})
+                    return JsonResponse({'error':'compilation', 'result':compilation.stderr+'\n'})
 
                 output = subprocess.run([executable], capture_output=True, text=True)
 
                 if output.returncode != 0:
-                    return JsonResponse({'error':'run', 'result':output.stderr})
+                    return JsonResponse({'error':'run', 'result':output.stderr+'\n'})
 
-                return JsonResponse({'result':output.stdout})
+                return JsonResponse({'result':output.stdout+'\n\n'})
 
             else:
                 command = ['python', file]
                 output = subprocess.run(command, capture_output=True, text=True)
 
                 if output.returncode != 0:
-                    return JsonResponse({'error':'run', 'result':output.stderr})
+                    return JsonResponse({'error':'run', 'result':output.stderr+'\n'})
 
-                return JsonResponse({'result':output.stdout})
-
-
-
-
-
-
-
-
+                return JsonResponse({'result':output.stdout+'\n'})
--- templates/main/main.js
+++ templates/main/main.js
@@ -1,18 +1,25 @@
-var lang = 'python'
+var lang = 'cpp';
 
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/tomorrow_night");
-editor.session.setMode("ace/mode/python");
-editor.setValue("print('Hello World')");
+editor.session.setMode("ace/mode/c_cpp");
+editor.setValue('#include <iostream>\nusing namespace std;\n\nint main() {\n    cout << "Hello World";\n    return 0;\n}');
 document.getElementById('editor').style.fontSize='16px';
 document.getElementById('editor').style.marginTop='60px';
 
 var terminal = ace.edit("terminal");
 terminal.setTheme("ace/theme/tomorrow_night");
-terminal.session.setMode("ace/mode/python");
 document.getElementById('terminal').style.fontSize='16px';
 
 function runSuccess(response) {
+    if (response.error) {
+        if (response.error == 'compilation') {
+            terminal.insert("Ошибка во время компиляции программы:\n\n");
+        }
+        else if (response.error == 'run') {
+            terminal.insert("Ошибка во время работы программы:\n\n");
+        }
+    }
     terminal.insert(response.result);
 }
 
@@ -34,13 +41,36 @@ function run(e) {
     });
 }
 
-function changeLang(choice) {
-    lang = choice;
-    if (choice == 'python') {
-        editor.session.setMode('ace/mode/python');
+function changeLang(id, choice) {
+    let parent = document.getElementById('lang');
+    parent.insertBefore(parent.children[1], parent.children[0]);
+    if (id == 1) {
+        parent.children[0].innerHTML = "Python ▼";
+        parent.children[1].innerHTML = "C/C++";
     }
     else {
-        editor.session.setMode('ace/mode/c_cpp');
+        parent.children[0].innerHTML = "C/C++ ▼";
+        parent.children[1].innerHTML = "Python";
+    }
 
+    lang = choice;
+    if (choice == 'cpp') { editor.session.setMode('ace/mode/c_cpp'); }
+    else { editor.session.setMode('ace/mode/python'); }
+}
+
+function changeTheme(id, choice) {
+    let parent = document.getElementById('theme');
+    parent.insertBefore(parent.children[1], parent.children[0]);
+    if (id == 1) {
+        parent.children[0].innerHTML = "White ▼";
+        parent.children[1].innerHTML = "Dark";
+        editor.setTheme("ace/theme/tomorrow");
+        terminal.setTheme("ace/theme/tomorrow");
+    }
+    else {
+        parent.children[0].innerHTML = "Dark ▼";
+        parent.children[1].innerHTML = "White";
+        editor.setTheme("ace/theme/tomorrow_night");
+        terminal.setTheme("ace/theme/tomorrow_night");
     }
 }
